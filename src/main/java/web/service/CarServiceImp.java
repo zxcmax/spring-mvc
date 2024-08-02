68892007661b54a18482e5c00f2749b4d2dd2e17
zxcmax <maks29121@gmail.com>
2024-08-02 15:47:09 +0700

    Refactor CarService

--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,12 +1,12 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.models.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Service
 public class CarServiceImp implements CarService {
     private static int CARS_COUNT;
     private List<Car> cars;
